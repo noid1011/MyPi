38c27a6034acc7a0fa4516659308419892c2dd29
noid1011 <noid1011@users.noreply.github.com>
2015-08-02 15:07:59 +0100

    Create random_colors.py

---
+++ random_colors.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python
+from phue import Bridge
+import random
+
+b = Bridge() # Enter bridge IP here.
+
+#If running for the first time, press button on bridge and run with b.connect() uncommented
+#b.connect()
+
+lights = b.get_light_objects()
+
+for light in lights:
+  b.set_light(light,'on', True)
+	light.brightness = 254
+	light.xy = [random.random(),random.random()]
