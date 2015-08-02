a984ba049798b0beb7c3ff3fa9e0025fe94eac6b
noid1011 <noid1011@users.noreply.github.com>
2015-08-02 16:04:26 +0100

    Update and rename pause.py to pause.sh

--- AirPlay-Control/pause.sh
+++ AirPlay-Control/pause.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 curl "http://localhost:3689/login?pairing-guid=0x1&request-session-id=50"
 curl "http://localhost:3689/ctrl-int/1/playpause?session-id=50"
 curl "http://localhost:3689/logout?session-id=50"
