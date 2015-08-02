b53bef5228a55fd3c1a0ea9e6707dcb5cc1f75b3
noid1011 <noid1011@users.noreply.github.com>
2015-08-02 16:37:10 +0100

    Update pause.sh

--- AirPlay-Control/pause.sh
+++ AirPlay-Control/pause.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-curl "http://localhost:3689/login?pairing-guid=0x1&request-session-id=50"
-curl "http://localhost:3689/ctrl-int/1/playpause?session-id=50"
-curl "http://localhost:3689/logout?session-id=50"
+curl "http://localhost:3689/login?pairing-guid=0x1&request-session-id=50" > /dev/null 2>&1
+curl "http://localhost:3689/ctrl-int/1/playpause?session-id=50" > /dev/null 2>&1
+curl "http://localhost:3689/logout?session-id=50" > /dev/null 2>&1
