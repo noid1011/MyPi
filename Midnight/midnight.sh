892d644b0d3cb5be1491905ddfa415dec7a36016
noid1011 <noid1011@users.noreply.github.com>
2015-12-30 15:16:24 +0000

    Update midnight.sh

--- Midnight/midnight.sh
+++ Midnight/midnight.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
+# shutdown fire tv
+#adb kill-server
+#adb start-server
+#adb connect 192.168.0.21
+#adb shell input keyevent 26
+#adb kill-server
 
-adb kill-server
-adb start-server
-adb connect 192.168.0.21
-adb shell input keyevent 26
-adb kill-server
+#trim log files
+echo "`tail -2000 ~/logs/whoshome.log`" > ~/logs/whoshome.log
