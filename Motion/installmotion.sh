a9c5652ee31685c7afd15d6ba177bdb4b8ce178d
noid1011 <noid1011@users.noreply.github.com>
2016-01-04 09:19:55 +0000

    Update installmotion.sh

--- Motion/installmotion.sh
+++ Motion/installmotion.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 sudo apt-get -y install git
+sudo apt-get install dh-autoreconf
+
 sudo apt-get remove x264, libx264-dev, libavcodec, libavcodec, libavfilter, libavfilter, libavresample, libavutil, libpostproc, libswresample, libswscale, ffmpeg
 cd ~
 
@@ -8,16 +10,23 @@ git clone git://git.videolan.org/x264.git
 cd x264
 ./configure --enable-shared --disable-opencl
 make
-sudo make install && ldconfig
+sudo make install 
+sudo ldconfig
+cd ~
 
 git clone git://source.ffmpeg.org/ffmpeg.git
 cd ffmpeg
 ./configure --arch=armhf --target-os=linux --enable-gpl --enable-libx264 --enable-nonfree --enable-shared
 make
-sudo make install && ldconfig
+sudo make install 
+sudo ldconfig
+cd ~
 
 git clone https://github.com/Mr-Dave/motion.git
 cd motion
+autoreconf -fiv
 ./configure
 make
-sudo make install && ldconfig 
+sudo make install 
+sudo ldconfig 
+cd ~
